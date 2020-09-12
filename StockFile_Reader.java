2bda227e0cf070b3e8c19ecd86ac8045613f4b05
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-12 11:49:04 +0530

    Update StockFile_Reader.java

--- StockFile_Reader.java
+++ StockFile_Reader.java
@@ -1,1 +1,61 @@
+public class Stockfilereader
+{
 
+  String filePath = null;
+  public Stockfilereader(String filepath)
+  {
+    this.filepath = filepath;
+  }
+  public List<String>getHeaders()throws IOException
+  {
+   String line = readfirstline(filepath);
+    String[]header = line.split(",");
+    List<String>values = new Arraylist<String>();
+    values = Arrays.aslist(header);
+    return values;
+  
+  }
+  static String readfirstline(String path)throws IOException
+  {
+  
+  try(BufferReader br =
+                  new BufferReader(new FileReader(path)))
+  {
+  
+      return br.readline();
+  
+  
+  }
+    
+  
+  
+  }    
+   public List<String> readfiledata()throws IOException
+   
+   
+   {
+     List<String>lines = new ArrayList<String>();
+     try(BufferedReader br = new Bufferedreader(new filereader(filepath)))
+     {
+        br.readline();
+        String line = null;
+         while(line=br.readline())!=null)
+         {
+          lines.add(line);
+         
+         }
+     }
+   
+   return lines;
+   
+   
+   
+   }
+     
+
+
+
+
+
+
+}
